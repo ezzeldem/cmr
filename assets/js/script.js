c07b45afccf0564d12349ea335a93febb17c7621
ezzelden <ezzelden06@gmail.com>
2020-10-12 22:54:55 +0200

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
+ 
 
+      // open menu
+      $(".menu-icon i ").click(function () {
+        $(".nav-section .nav-ul").addClass("left-0");
+        $("body").addClass("body-over-lay");
+        $(".over-lay").show();
+    });
+
+    // close menu
+    $(".close-menu i, .over-lay").click(function () {
+        $(".nav-section .nav-ul").removeClass("left-0");
+        $("body").removeClass("body-over-lay");
+        $(".over-lay").hide();
+    });
+
+
+
+
+
+    // blog slider
     $(".blog-slider").slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 3000,
         dots: true,
         arrows:false,
@@ -12,10 +32,12 @@ $(document).ready(function(){
         centerMode:true,
         centerPadding: '0px',
         responsive: [
+            
             {
                 breakpoint: 992,
                 settings: {
                     slidesToShow: 2,
+                    centerMode:false,
                     slidesToScroll: 1,
                 },
             },
@@ -23,10 +45,10 @@ $(document).ready(function(){
                 breakpoint: 772,
                 settings: {
                     slidesToShow: 1,
+                    centerMode:false,
                     slidesToScroll: 1,
-                    arrows: false,
                 },
-            },
+            }
         ],
     });
 
